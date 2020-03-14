b6aef065530d706639813718b72382c98856b3e5
Chengwei Zhang <1535909927@qq.com>
2020-03-14 21:37:56 +0800

    add note.

--- 19v2.cpp
+++ 19v2.cpp
@@ -2,6 +2,18 @@
 // Created by Chengwei Zhang on 3/13/20.
 //
 
+/*
+ * 思路：
+ * 要找到需要被删除的节点的前一个节点。
+ * 如果要被删除的节点是head，则它没有前一个节点。
+ * 因此，通过dummy节点（指向head）来解决。
+ *
+ * 然后使用双指针，都从dummy节点出发。
+ * 将第一个指针向前n + 1步，然后停止。
+ * 同时移动两个指针，直到第一个指针移动到尾节点为止。
+ * 此时，第二个指针指向的就是要被删除的节点的前一个节点。
+ */
+
 #include <iostream>
 using namespace std;
 
@@ -14,30 +26,21 @@ struct ListNode {
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* p = head;
-        int len = 0;
-        while (p != NULL) {
-            len ++;
-            p = p -> next;
+        ListNode* dummy = new ListNode(0);
+        dummy -> next = head;
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+        for (int i = 1; i <= n + 1; i ++) {
+            fast = fast -> next;
         }
-        if (n == len) {
+        if (fast == NULL) {
             return head -> next;
         }
-        else {
-            p = head;
-            int current = 0;
-            ListNode* q;
-            while (current != len - n - 1) {
-                p = p -> next;
-                current ++;
-            }
-            if (n == 1) {
-                p -> next = NULL;
-            }
-            else {
-                p -> next = p -> next -> next;
-            }
+        while (fast != NULL) {
+            fast = fast -> next;
+            slow = slow -> next;
         }
+        slow -> next = slow -> next -> next;
         return head;
     }
 };
