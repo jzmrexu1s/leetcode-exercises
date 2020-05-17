dad28c95405d17c708979a1b664189b395e0cf6d
Chengwei Zhang <1535909927@qq.com>
2020-05-18 01:14:27 +0800

    add 61.

---
+++ In-place Reversal of LL/61.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Chengwei Zhang on 5/18/20.
+//
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (head == NULL) return head;
+        int len = 1;
+        ListNode* p = head;
+        while (p != NULL && p -> next != NULL) {
+            len ++;
+            p = p -> next;
+        }
+        // cout << p -> val << endl;
+        p -> next = head;
+        // cout << p -> next -> val << endl;
+        int shift = len - k % len;
+        p = head;
+        while (shift > 0) {
+            p = p -> next;
+            shift --;
+        }
+        ListNode *start = p;
+        // cout << p -> val << endl;
+        while (len > 1) {
+            p = p -> next;
+            len --;
+        }
+        p -> next = NULL;
+        return start;
+
+    }
+};
