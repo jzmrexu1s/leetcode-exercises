9a5eb71f4fa2e15eb8c4d86b1d35d0214a957ae1
Chengwei Zhang <1535909927@qq.com>
2020-07-05 17:09:45 +0800

    add 152.

--- Dynamic Programming/152. Maximum Product Subarray.cpp
+++ Dynamic Programming/152. Maximum Product Subarray.cpp
@@ -2,3 +2,21 @@
 // Created by Chengwei Zhang on 7/4/20.
 //
 
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int sz = nums.size();
+        if (sz == 0) return 0;
+        int* dpMin = new int[sz];
+        int* dpMax = new int[sz];
+        dpMin[0] = nums[0];
+        dpMax[0] = nums[0];
+        int maxVal = nums[0];
+        for (int i = 1; i < sz; i ++) {
+            dpMin[i] = min(nums[i], min(dpMin[i - 1] * nums[i], dpMax[i - 1] * nums[i]));
+            dpMax[i] = max(nums[i], max(dpMin[i - 1] * nums[i], dpMax[i - 1] * nums[i]));
+            maxVal = max(dpMax[i], maxVal);
+        }
+        return maxVal;
+    }
+};
